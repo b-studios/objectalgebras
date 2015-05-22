37fef72508c44304bb67aa8cc8820c505fb8f235
Jonathan Brachthaeuser <jonathan@b-studios.de>
2015-05-22 10:40:58 +0200

    Refactored to more structured macro bundle

--- core/src/main/scala/objectalgebras/syntax/attribute.scala
+++ core/src/main/scala/objectalgebras/syntax/attribute.scala
@@ -3,65 +3,91 @@ package syntax
 
 import scala.annotation.implicitNotFound
 import scala.language.experimental.macros
+import scala.language.reflectiveCalls
 import scala.annotation.StaticAnnotation
 import scala.reflect.macros.whitebox.Context
-
 
 package object attribute extends AttributeFallbacks {
 
   class attr extends StaticAnnotation {
-    def macroTransform(annottees: Any*): Any = macro attrImpl
+    def macroTransform(annottees: Any*): Any = macro AttrAnnotationMacro.apply
   }
 
-  def attrImpl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+  class AttrAnnotationMacro(val c: Context) {
     import c.universe._
+    import c.Expr
 
-    val (attrName, attrType) = annottees.head.tree match {
+    def apply(annottees: Expr[Any]*): Expr[Any] = {
+      val (name, tpe) = processInput(annottees.head.tree)
+      Expr(Attr(Names(name), tpe).gen)
+    }
+
+    def processInput(t: Tree): (TermName, Tree) = t match {
       case q"def $name: $tpe" => (name, tpe)
       case q"def $name[$tArgs]: $tpe" =>
-        c.abort(c.enclosingPosition, "parametrized attributes are not yet supported")
+        c.abort(c.enclosingPosition,
+          "parametrized attributes are not yet supported")
       case t =>
-        c.abort(c.enclosingPosition, s"Wrong shape for an attribute definition\n  Expected something like: `@attr def foo: Bar`")
+        c.abort(c.enclosingPosition,
+          s"""Wrong shape for an attribute definition
+             |  Expected something like: `@attr def foo: Bar`""".stripMargin)
     }
 
-    object names {
-      val str = attrName.toString
-      val cap = str.capitalize
-      val cls = TypeName(cap)
-      val cmp = TermName(cap)
-      val prm = TermName(c.freshName("x"))
-      val low = TypeName(s"${cap}Fallbacks")
-      val fallback = TypeName(s"${cap}AccessFallback")
-      val err = TypeName(s"${cap}AccessError")
+    case class Names(in: TermName) {
+      // strings
+      val name = in.toString
+      val cap  = name.capitalize
+
+      // terms
+      val attr      = in
+      val companion = TermName(cap)
+      val param     = TermName(c.freshName("x"))
+
+      // types
+      val cls       = TypeName(cap)
+      val low       = TypeName(s"${cap}Fallbacks")
+      val fallback  = TypeName(s"${cap}AccessFallback")
+      val err       = TypeName(s"${cap}AccessError")
     }
 
-    val memberNotFoundErrorMessage =
-      s"""|Method '${names.str}' cannot be invoked:
-          |  '${names.str}' is not available on the node of type '$${E}'.
-          |  Try adding '${names.cap}' as dependency to your object algebra.""".stripMargin
+    case class Attr(names: Names, resultType: Tree) {
 
-    val res = q"""trait ${names.cls} extends Attribute {
-      def $attrName: $attrType
+      lazy val memberNotFoundErrorMessage =
+        s"""|Method '${names.name}' cannot be invoked:
+            |  '${names.name}' is not available on the node of type '$${E}'.
+            |  Try adding '${names.cap}' as dependency to your object algebra.""".stripMargin
+
+
+      lazy val genTrait = q"""
+        trait ${names.cls} extends Attribute {
+          def ${names.attr}: $resultType
+        }
+      """
+
+      lazy val genCompanion = q"""
+        object ${names.companion} extends ${names.low} {
+          // this really should be a macro to avoid forcing the evaluation!
+          implicit def ${names.attr}(${names.param} : $resultType): ${names.cls}
+            = new ${names.cls} {
+              def ${names.attr}: $resultType = ${names.param}
+            }
+        }
+      """
+
+      lazy val genLowPrioImplicits = q"""
+        trait ${names.low} {
+          @annotation.implicitNotFound(msg = $memberNotFoundErrorMessage)
+          sealed trait ${names.err}[E]
+
+          implicit class ${names.fallback}[A <: Attribute](a: A) {
+            def ${names.attr}(implicit err: ${names.err}[A]): $resultType = ???
+          }
+        }
+      """
+
+      lazy val gen = q"""$genTrait; $genCompanion; $genLowPrioImplicits"""
     }
-    object ${names.cmp} extends ${names.low} {
-      implicit def $attrName(${names.prm} : $attrType): ${names.cls} = new ${names.cls} {
-        def $attrName: $attrType = ${names.prm}
-      }
-    }
-    trait ${names.low} {
-      @annotation.implicitNotFound(msg = $memberNotFoundErrorMessage)
-      sealed trait ${names.err}[E]
-
-      implicit class ${names.fallback}[A <: Attribute](a: A) {
-        def $attrName(implicit err: ${names.err}[A]): $attrType = ???
-      }
-
-    }
-    """
-
-    c.Expr(res)
   }
-
 }
 
 /**
