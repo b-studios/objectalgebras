4c351bf98dc0f684da6f136e702126564e6a0d0e
Jonathan Brachthaeuser <jonathan@b-studios.de>
2015-05-22 19:09:13 +0200

    Added small example using both attr and sig

---
+++ examples/src/main/scala/objectalgebras/simplesigandattr.scala
@@ -0,0 +1,23 @@
+package objectalgebras
+package tests
+
+import syntax.attribute._
+import syntax.signature._
+
+object signaturesAndAttributes {
+
+  @attr def eval: Int
+
+  @sig trait Expr {
+    def Lit: Int
+    def Add: (Expr, Expr)
+  }
+
+  object evalAlg extends Expr[Eval, Eval] {
+    def Lit = identity
+    def Add = (l, r) => l.eval + r.eval
+  }
+
+  val foldable: Expr.Complete[Eval] = evalAlg
+
+}
